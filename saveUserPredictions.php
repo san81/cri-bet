cf6422f6e1a0c8310243bc36696cd6066e8dbaff
santhosh.gandhe <santhosh.gandhe@f65db912-5131-11de-935c-5df29b8bbac0>
2009-06-04 19:10:41 +0000

    saving user predictions added

--- saveUserPredictions.php
+++ saveUserPredictions.php
@@ -3,4 +3,30 @@
  * page to save user predictions
  */
 
+include('connect.php');
+$matchId = $_POST['matchId'];
+$userID = 1;
+
+$query = "SELECT * FROM questions";
+$result_set_obj = mysql_query($query);
+$result_set = mysql_fetch_array($result_set_obj);
+$i=0;
 ?>
+
+<?php
+while($i<mysql_num_rows($result_set_obj)){
+    $qid = $result_set[0];
+    $userChoice = $_POST['q'.$qid];
+    $betAmt = $_POST['bet'.$qid];
+    $insert_query = "insert into user_bettings
+        (match_id,qid,user_id,user_choice,bet_amount)
+     values ($matchId,$qid,$userID,$userChoice,$betAmt)";
+    echo $insert_query;
+    mysql_query($insert_query);
+
+    $i++;
+    $result_set = mysql_fetch_array($result_set_obj);
+}
+mysql_close();
+?>
+Your bettins are saved. Thankyou
